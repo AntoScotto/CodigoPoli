6da8cf8f2496b2e20625c3942f63c43a578e0f28
anto <antonelascotto@gmail.com>
2024-05-13 20:08:46 -0300

    Funciones para Py

---
+++ funciones.py
@@ -0,0 +1,81 @@
+#funciones en py
+#retorna el valor absoluto del parametro que le pase
+'''
+n=-12
+print('el resultado de abs es:',abs(n))
+'''
+'''
+#obtiene la conversion a binario
+b=3
+print('el resultado de bin es:',bin(b))
+'''
+'''
+#retornar el caracter en formato unicode
+c=200
+print('el resultado de chr es:',chr(c))
+'''
+'''
+#retornar el cociente y el resto de una division
+
+d,e=14,7
+cociente,resto=divmod(d,e)
+print('el resultado de divmod:', cociente,'y el resto:',resto)
+'''
+'''
+#Convierte una cadena de txt a dicmal 
+f='23.54'
+print('el resultado de la funcion float es ',float(f))
+'''
+'''
+#obyoene la version a hexadecimal
+g=124
+print('el resultado de la funcion hex es:',hex(g))
+'''
+'''
+#le pide al usuario que ingrese una string
+h=input('ingrese su nombre:')
+print('el resultado de la funcion input es:',h)
+'''
+'''
+#convierte la cadena de txt a un numero entero
+i=123
+pirnt('El resultado de la funcion int es:', int(i))
+'''
+'''
+#Retorna la longitud del objeto to0mado por parametro
+j=12,23,45
+print('el resultado de la funcion len es: ',len(j))
+'''
+'''
+#Retorna el mayor de los parametros tomados
+k,l=21,43
+print('el resultado de la funcion max es:',max(k,l))
+#Retorna el mayor de los parametros tomados
+k,l=21,43
+print('el resultado de la funcion min es:',min(k,l))
+'''
+'''
+#obtiene la conversion a octal
+o=124
+print('El resultado de la funcion oct es:',oct(o))
+'''
+'''
+#retorna el entero unicode
+p='@'
+print('el resultado de la funcion ord es:',ord(p))
+'''
+'''
+#retorna la potencia entre dos numeros
+q,r=2,3
+print('el resultado de la funcion pow es:',pow(q,r))
+'''
+'''
+# redondea a la cantidad de decimales que quieran 
+s = 123.345345345
+print('El resultado de la funcion round es: ', round(s,2))
+'''
+'''
+#tupla
+t= 2,4,1
+print('el resultado de la funcion str es: ',str(t))
+'''
