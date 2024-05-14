906e014173fbab3c05ccd8fbe44c60908ba87c2f
AntoScotto <126833542+AntoScotto@users.noreply.github.com>
2024-05-14 17:15:19 -0300

    Update clasesiete.py

--- clasesiete.py
+++ clasesiete.py
@@ -1,18 +1,5 @@
 
-'''
-#ejercicio31
-nomes=input('nombre del estudiante:')
-nompro=input('nombre del docente:')
-n1=int(input('nota 1:'))
-n2=int(input('nota 2:'))
-n3=int(input('nota 3:'))
-t=n1,n2,n3
 
-print(nomes,'est.')
-print(nompro,'prof.')
-print('promedio notas:',t/3)
-#ver
-'''
 '''
 #ejercicio32
 a=int(input('edad 1:'))
