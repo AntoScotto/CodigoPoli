5cd95c5c0f7da03253f21c8289a97075bdd05e14
anto <antonelascotto@gmail.com>
2024-05-13 21:17:33 -0300

    ejercicio clase 13 de mayo

---
+++ clasesiete.py
@@ -0,0 +1,34 @@
+
+'''
+#ejercicio31
+nomes=input('nombre del estudiante:')
+nompro=input('nombre del docente:')
+n1=int(input('nota 1:'))
+n2=int(input('nota 2:'))
+n3=int(input('nota 3:'))
+t=n1,n2,n3
+
+print(nomes,'est.')
+print(nompro,'prof.')
+print('promedio notas:',t/3)
+#ver
+'''
+'''
+#ejercicio32
+a=int(input('edad 1:'))
+b=int(input('edad 2:'))
+print('El mayor es ',max(a,b))
+'''
+
+'''
+cad1='hola'
+cad2='mundo'
+if cad1 == cad2:
+    print('son iguales')
+else:
+    print('no son iguales')
+if cad1 != 'hello':
+    print('No es la palabra Hello')
+else:
+    print('Es la palabra Hello')
+    '''
