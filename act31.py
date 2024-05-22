e71dd9cea67595e0b10415ba07ca1b423d6f6508
AntoScotto <126833542+AntoScotto@users.noreply.github.com>
2024-05-22 19:58:06 -0300

    Update act31.py

--- act31.py
+++ act31.py
@@ -3,9 +3,13 @@
 nomes=input('nombre del estudiante: ')
 nompro=input('nombre del docente: ')
 pro=float(input('ingrese promedio del estudiante: '))
-
+#forma1
 t1='est.','prof.'
-
 print(nomes,t1[0])
 print(nompro,t1[1])
 print('promedio notas:',round(pro))
+
+#forma 2
+
+datos=(nomes,nompro,pro)
+print('est.',datos[0],'prof.',datos[1],'promedio:',round(pro))
